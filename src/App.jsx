ab88665bc4ae6cde070428b9537a2eba3be7e753
Sirpreet Kaur Dhillon <sirpreet.dhillon@gmail.com>
2024-11-12 10:37:26 -0800

    Comming Soon Page

--- src/App.jsx
+++ src/App.jsx
@@ -9,25 +9,11 @@ function App() {
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <h1>Sirpreet Dhillon</h1>
+      <h3>Portfolio</h3>
+      <h6>Coming Soon</h6>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      
     </>
   )
 }
